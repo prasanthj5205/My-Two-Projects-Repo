b3c6531564e7466aece52fc0049bfc293c2b89b2
Prasanth J <prasanthj5205@gmail.com>
2024-12-30 13:29:21 -0600

    Pushed SpringOAuthDemo Porject

---
+++ SpringOAuthDemo/src/main/java/com/jp/SpringOAuthDemo/Controller/OAuthController.java
@@ -0,0 +1,13 @@
+package com.jp.SpringOAuthDemo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OAuthController {
+
+    @GetMapping("/greet")
+    public String greetMessage(){
+        return "Hello Spring OAuth Client";
+    }
+}
